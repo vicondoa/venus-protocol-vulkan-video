b7d9f9389b8978d566efec5017d7017623617f49
Chia-I Wu <olvaffe@gmail.com>
2020-10-21 12:04:33 -0700

    use helpers in driver_calls.h

--- templates/driver_calls.h
+++ templates/driver_calls.h
@@ -10,21 +10,33 @@ static inline ${ty.c_func_ret()} vn_call_${ty.name}(struct vn_instance *vn_insta
     const size_t reply_size = vn_sizeof_${ty.name}_reply(${ty.c_func_args()});
     const VnCommandFlags cmd_flags = VN_COMMAND_GENERATE_REPLY_BIT;
 
-    mtx_lock(&vn_instance->mutex);
+    struct vn_cs *cs = vn_instance_lock_cs(vn_instance);
 
-    /* TODO suballocate from reply stream and avoid locking */
-    assert(reply_size <= vn_instance->reply_size);
+    size_t reply_offset;
+    void *reply_ptr;
+    struct vn_renderer_bo *reply_bo = vn_instance_alloc_cs_reply_locked(
+            vn_instance, reply_size, &reply_offset, &reply_ptr);
+    if (!reply_bo) {
+       vn_instance_unlock_cs(vn_instance);
+%   if ty.ret:
+      return VK_ERROR_OUT_OF_HOST_MEMORY;
+%   else:
+      return;
+%   endif
+    }
 
     /* TODO too many commands... */
-    vn_cs_out_begin_reply_stream(&vn_instance->cs);
-    if (vn_cs_reserve_out(&vn_instance->cs, cmd_size))
-        vn_encode_${ty.name}(&vn_instance->cs, cmd_flags, ${ty.c_func_args()});
-    vn_cs_out_end_reply_stream(&vn_instance->cs, vn_instance->reply_bo->res_id, 0, reply_size);
+    vn_cs_out_begin_reply_stream(cs);
+    if (vn_cs_reserve_out(cs, cmd_size))
+        vn_encode_${ty.name}(cs, cmd_flags, ${ty.c_func_args()});
+    vn_cs_out_end_reply_stream(cs, reply_bo->res_id, reply_offset, reply_size);
 
-   if (vn_cs_has_error(&vn_instance->cs)) {
-      vn_cs_reset(&vn_instance->cs);
-      vn_cs_set_error(&vn_instance->cs);
-      mtx_unlock(&vn_instance->mutex);
+   if (vn_cs_has_error(cs)) {
+      vn_cs_reset(cs);
+      vn_cs_set_error(cs);
+      vn_instance_unlock_cs(vn_instance);
+      vn_instance_free_cs_reply(vn_instance, reply_bo);
+
 %   if ty.ret:
       return VK_ERROR_OUT_OF_HOST_MEMORY;
 %   else:
@@ -32,22 +44,24 @@ static inline ${ty.c_func_ret()} vn_call_${ty.name}(struct vn_instance *vn_insta
 %   endif
    }
 
-    vn_cs_end_out(&vn_instance->cs);
+    vn_cs_end_out(cs);
 
-    /* TODO suballocate reply_bo to avoid round trip with lock held... */
-    vn_renderer_submit(vn_instance->renderer, &vn_instance->cs, &vn_instance->reply_bo, 1, true);
+    /* TODO replace wait_cpu by vn_renderer_sync to wait without lock */
+    vn_renderer_submit(vn_instance->renderer, cs, &reply_bo, 1, true);
 
-    vn_cs_set_in_data(&vn_instance->cs, vn_instance->reply_ptr, vn_instance->reply_size);
+    /* TODO use a local cs to decode without lock */
+    vn_cs_set_in_data(cs, reply_ptr, reply_size);
 
 %   if ty.ret:
-    const ${ty.ret.to_c()} = vn_decode_${ty.name}_reply(&vn_instance->cs, ${ty.c_func_args()});
+    const ${ty.ret.to_c()} = vn_decode_${ty.name}_reply(cs, ${ty.c_func_args()});
 %   else:
-    vn_decode_${ty.name}_reply(&vn_instance->cs, ${ty.c_func_args()});
+    vn_decode_${ty.name}_reply(cs, ${ty.c_func_args()});
 %   endif
 
-    vn_cs_reset(&vn_instance->cs);
+    vn_cs_reset(cs);
 
-    mtx_unlock(&vn_instance->mutex);
+    vn_instance_unlock_cs(vn_instance);
+    vn_instance_free_cs_reply(vn_instance, reply_bo);
 %   if ty.ret:
 
     return ${ty.ret.name};
@@ -61,10 +75,10 @@ static inline void vn_async_${ty.name}(struct vn_instance *vn_instance, ${ty.c_f
     const size_t cmd_size = vn_sizeof_${ty.name}(${ty.c_func_args()});
     const VnCommandFlags cmd_flags = 0;
 
-    mtx_lock(&vn_instance->mutex);
-    if (vn_cs_reserve_out(&vn_instance->cs, cmd_size))
-        vn_encode_${ty.name}(&vn_instance->cs, cmd_flags, ${ty.c_func_args()});
-    mtx_unlock(&vn_instance->mutex);
+    struct vn_cs *cs = vn_instance_lock_cs(vn_instance);
+    if (vn_cs_reserve_out(cs, cmd_size))
+        vn_encode_${ty.name}(cs, cmd_flags, ${ty.c_func_args()});
+    vn_instance_unlock_cs(vn_instance);
 }
 </%def>\
 \
@@ -83,25 +97,25 @@ ${async_command(ty)}
 static inline void
 vn_async_flush(struct vn_instance *instance)
 {
-   mtx_lock(&instance->mutex);
+    struct vn_cs *cs = vn_instance_lock_cs(instance);
 
-   if (!vn_cs_has_out(&instance->cs)) {
-      mtx_unlock(&instance->mutex);
-      return;
-   }
+    if (!vn_cs_has_out(cs)) {
+        vn_instance_unlock_cs(instance);
+        return;
+    }
 
-   if (vn_cs_has_error(&instance->cs)) {
-      vn_cs_reset(&instance->cs);
-      vn_cs_set_error(&instance->cs);
-      mtx_unlock(&instance->mutex);
-      return;
-   }
+    if (vn_cs_has_error(cs)) {
+        vn_cs_reset(cs);
+        vn_cs_set_error(cs);
+        vn_instance_unlock_cs(instance);
+        return;
+    }
 
-   vn_cs_end_out(&instance->cs);
-   vn_renderer_submit(instance->renderer, &instance->cs, NULL, 0, false);
-   vn_cs_reset(&instance->cs);
+    vn_cs_end_out(cs);
+    vn_renderer_submit(instance->renderer, cs, NULL, 0, false);
+    vn_cs_reset(cs);
 
-   mtx_unlock(&instance->mutex);
+    vn_instance_unlock_cs(instance);
 }
 
 #endif /* VN_PROTOCOL_DRIVER_CALLS_H */
