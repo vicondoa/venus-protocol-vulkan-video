e60093dca9af7a77cbdcaef9abd4040dd52fc4f3
Yiwei Zhang <zzyiwei@chromium.org>
2021-04-19 21:58:46 +0000

    advertise VK_EXT_queue_family_foreign to the guest
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Reviewed-by: Chia-I Wu <olvaffe@gmail.com>

--- vn_protocol.py
+++ vn_protocol.py
@@ -84,6 +84,7 @@ VK_XML_EXTENSION_LIST = [
     # EXT extensions
     'VK_EXT_external_memory_dma_buf',
     'VK_EXT_image_drm_format_modifier',
+    'VK_EXT_queue_family_foreign',
     'VK_EXT_transform_feedback',
 ]
 
