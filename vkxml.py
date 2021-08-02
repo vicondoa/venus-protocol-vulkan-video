f3586175ee0945c5919ee753673a5f60f48f82c5
Chia-I Wu <olvaffe@gmail.com>
2021-08-02 12:56:44 -0700

    consider varaibles with 'len' optional
    No change to the generated code.

--- vkxml.py
+++ vkxml.py
@@ -107,6 +107,9 @@ class VkVariable:
         return self.ty.is_pointer() and self.is_optional()
 
     def is_optional(self):
+        # the length is determined by another variable
+        if 'len_exprs' in self.attrs:
+            return True
         return 'optional' in self.attrs and \
                 self.attrs['optional'][0] == 'true'
 
