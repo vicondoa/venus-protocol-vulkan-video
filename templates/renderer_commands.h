b54ad7932c7dc928a809a0a6e5a7c90812f7ea2b
Yiwei Zhang <zzyiwei@chromium.org>
2023-01-09 14:44:28 -0800

    fix clang warning for unrecognized -Wdangling-pointer
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

--- templates/renderer_commands.h
+++ templates/renderer_commands.h
@@ -52,7 +52,9 @@ static inline void vn_dispatch_${ty.name}(struct vn_dispatch_context *ctx, VkCom
 % endif
 
 #pragma GCC diagnostic push
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wdangling-pointer"
+#endif
 #pragma GCC diagnostic ignored "-Wpointer-arith"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
