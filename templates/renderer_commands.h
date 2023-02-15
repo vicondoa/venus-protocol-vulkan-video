96bda9cc85d55a9e29a2cff9cf2b21485757cdb1
Yiwei Zhang <zzyiwei@chromium.org>
2023-02-15 10:29:54 -0800

    fix dangling pointer warning for gcc < 12
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

--- templates/renderer_commands.h
+++ templates/renderer_commands.h
@@ -52,7 +52,7 @@ static inline void vn_dispatch_${ty.name}(struct vn_dispatch_context *ctx, VkCom
 % endif
 
 #pragma GCC diagnostic push
-#ifndef __clang__
+#if !defined(__clang__) && defined(__GNUC__) && __GNUC__ >= 12
 #pragma GCC diagnostic ignored "-Wdangling-pointer"
 #endif
 #pragma GCC diagnostic ignored "-Wpointer-arith"
