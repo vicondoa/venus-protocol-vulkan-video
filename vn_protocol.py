1edb4daf932c1099f17f1dd7fc2d3a1d6bb7bf86
Yiwei Zhang <zzyiwei@chromium.org>
2023-05-16 21:34:48 -0700

    add VK_EXT_image_2d_view_of_3d support
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/virgl/venus-protocol/-/merge_requests/72>

--- vn_protocol.py
+++ vn_protocol.py
@@ -117,6 +117,7 @@ VK_XML_EXTENSION_LIST = [
     'VK_EXT_depth_clip_control',
     'VK_EXT_depth_clip_enable',
     'VK_EXT_external_memory_dma_buf',
+    'VK_EXT_image_2d_view_of_3d',
     'VK_EXT_image_drm_format_modifier',
     'VK_EXT_image_view_min_lod',
     'VK_EXT_index_type_uint8',
