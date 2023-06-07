d625da34496210dba9cacc60e3856e277a706de5
Juston Li <justonli@google.com>
2023-06-07 14:34:54 -0700

    add VK_KHR_shader_clock
    Signed-off-by: Juston Li <justonli@google.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/venus-protocol/-/merge_requests/74>

--- vn_protocol.py
+++ vn_protocol.py
@@ -109,6 +109,7 @@ VK_XML_EXTENSION_LIST = [
     'VK_KHR_external_memory_fd',
     'VK_KHR_external_semaphore_fd',
     'VK_KHR_push_descriptor',
+    'VK_KHR_shader_clock',
     # EXT extensions
     'VK_EXT_calibrated_timestamps',
     'VK_EXT_conditional_rendering',
