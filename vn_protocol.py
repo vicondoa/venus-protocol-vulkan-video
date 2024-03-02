c1c8f6d9c24520bd8c2b15bee31f84c98215c60b
Yiwei Zhang <zzyiwei@chromium.org>
2024-03-01 20:17:30 -0800

    add VK_KHR_fragment_shading_rate
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/virgl/venus-protocol/-/merge_requests/82>

--- vn_protocol.py
+++ vn_protocol.py
@@ -108,6 +108,7 @@ VK_XML_EXTENSION_LIST = [
     'VK_KHR_external_fence_fd',
     'VK_KHR_external_memory_fd',
     'VK_KHR_external_semaphore_fd',
+    'VK_KHR_fragment_shading_rate',
     'VK_KHR_pipeline_library',
     'VK_KHR_push_descriptor',
     'VK_KHR_shader_clock',
