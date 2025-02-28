a25450b73cf8ac9bdefaad3bc0a5359ec5b7118d
Yiwei Zhang <zzyiwei@chromium.org>
2025-02-27 17:48:27 -0800

    add VK_KHR_deferred_host_operations
    For now, it's mainly for the protocol to gain the capability to handle
    VkDeferredOperationKHR object encoding and decoding. The extension
    itself has a lot of potential in venus-protocol implementations:
    
    1. It could be a no-op impl on the driver side. Basically the driver
       would immediately passthrough the expensive host cmds to the renderer
       side with a null deferred operation handle.
    2. It could be a pure driver side impl. The driver side can batch all
       the host cmds inside the deferred op first. Upon application
       invocation of the join cmd in its thread pool, the driver can spawn
       TLS venus ring to passthrough the deferred cmds to the corresponding
       host ring thread for parallel execution. The good thing here is all
       host cmds sent can be made asynchronous over the venus ring backing
       the app managed thread.
    3. It could be a renderer side impl. The deferred operation is created
       with the renderer side Vulkan impl. The driver would passthrough all
       expensive host cmds to the renderer along with the deferred op obj.
       Upon join, there's a single venus ring spawn for the deferred op to
       initiate the renderer side thread pool and monitor the result status
       of the renderer side parallel execution.
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/virgl/venus-protocol/-/merge_requests/98>

--- vn_protocol.py
+++ vn_protocol.py
@@ -125,6 +125,7 @@ VK_XML_EXTENSION_LIST = [
     # KHR extensions
     'VK_KHR_calibrated_timestamps',
     'VK_KHR_compute_shader_derivatives',
+    'VK_KHR_deferred_host_operations',
     'VK_KHR_depth_clamp_zero_one',
     'VK_KHR_external_fence_fd',
     'VK_KHR_external_memory_fd',
