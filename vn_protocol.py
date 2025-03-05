548a9c5fa1bdbbca5d89cc60a29ca8d39eecdc19
Yiwei Zhang <zzyiwei@chromium.org>
2025-03-05 00:58:24 -0800

    support VK_KHR_ray_tracing_pipeline
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/virgl/venus-protocol/-/merge_requests/98>

--- vn_protocol.py
+++ vn_protocol.py
@@ -135,6 +135,7 @@ VK_XML_EXTENSION_LIST = [
     'VK_KHR_fragment_shading_rate',
     'VK_KHR_pipeline_library',
     'VK_KHR_ray_query',
+    'VK_KHR_ray_tracing_pipeline',
     'VK_KHR_shader_clock',
     'VK_KHR_shader_maximal_reconvergence',
     'VK_KHR_shader_quad_control',
@@ -1500,7 +1501,9 @@ class GenStructsAndCommands:
         'pipeline': [
             'CreateComputePipeline',
             'CreateGraphicsPipeline',
+            'CreateRayTracingPipeline',
             'DestroyPipeline',
+            'GetRayTracing',
         ],
         'pipeline_layout': [
             'CreatePipelineLayout',
